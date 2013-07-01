8c40bbc10834049f28df04f5bf92c39c347815bb
Trevor Jin <ph_201@yahoo.com>
2013-07-01 02:31:59 -0500

    The first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GladioApp::Application.config.secret_key_base = 'b01de4093a678e3376d21fa8f5aee22e993283134ddd467c193ae222cc27c4b999a73c99a0e66447661fca0e7e1d5d8a88fcebcb1789f0b85f727a63de1643ce'
