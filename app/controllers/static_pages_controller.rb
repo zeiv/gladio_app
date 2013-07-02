16ff83351098a7824bab049fda6610c40f55ae1c
Trevor Jin <ph_201@yahoo.com>
2013-07-02 11:11:08 -0500

    Creates blog controller, view, and route. Fixes #14.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,19 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
-
-  def new
-  end
-
   def about
+  end
+
+  def blog
   end
 
   def contact
   end
 
   def help
+  end
+
+  def home
+  end
+
+  def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ GladioApp::Application.routes.draw do
   get "static_pages/about"
   get "static_pages/contact"
   get "static_pages/help"
+  get "static_pages/blog"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
