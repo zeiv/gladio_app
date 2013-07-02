a44428b12aa1b68f5dcf0ad4aff62f695b6ed536
Trevor Jin <ph_201@yahoo.com>
2013-07-02 11:02:26 -0500

    Organizes static pages spec and creates failing blog existence spec.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,32 +2,6 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
-
-    it "should have the content 'Gladio App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Gladio App')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Gladio | Home")
-    end
-  end
-
-  describe "New page" do
-
-    it "should have the content 'New'" do
-      visit '/static_pages/new'
-      expect(page).to have_content('New')
-    end
-
-    it "should have the title 'New'" do
-      visit '/static_pages/new'
-      expect(page).to have_title("Gladio | New")
-    end
-  end
-
   describe "About page" do
 
     it "should have the content 'About Us'" do
@@ -38,6 +12,19 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Gladio | About Us")
+    end
+  end
+
+  describe "Blog page" do
+
+    it "should have the content 'Gladio Blog'" do
+      visit '/static_pages/blog'
+      expect(page).to have_content('Gladio Blog')
+    end
+
+    it "should have the title 'Blog'" do
+      visit '/static_pages/blog'
+      expect(page).to have_title("Gladio | Blog")
     end
   end
 
@@ -64,6 +51,32 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_title("Gladio | Help")
+    end
+  end
+
+  describe "Home page" do
+
+    it "should have the content 'Gladio App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Gladio App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Gladio | Home")
+    end
+  end
+
+  describe "New page" do
+
+    it "should have the content 'New'" do
+      visit '/static_pages/new'
+      expect(page).to have_content('New')
+    end
+
+    it "should have the title 'New'" do
+      visit '/static_pages/new'
+      expect(page).to have_title("Gladio | New")
     end
   end
 end
