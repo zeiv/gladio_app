35ae6586e17db44ed925fe404b7a32a70d09608c
Trevor Jin <ph_201@yahoo.com>
2013-07-02 11:45:02 -0500

    Refactors the static pages spec to only visit each page one time.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,81 +1,65 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  subject { page }
 
   describe "About page" do
+    before { visit about_path}
 
-    it "should have the h1 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About Us') }
 
     it "should have the title 'About Us'" do
-      visit about_path
       expect(page).to have_title("Gladio | About Us")
     end
   end
 
   describe "Blog page" do
+    before { visit blog_path}
 
-    it "should have the h1 'Gladio Blog'" do
-      visit blog_path
-      expect(page).to have_content('Gladio Blog')
-    end
+    it { should have_content('Gladio Blog') }
 
     it "should have the title 'Blog'" do
-      visit blog_path
       expect(page).to have_title("Gladio | Blog")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path}
 
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
+    it { should have_content('Contact') }
 
     it "should have the title 'Contact Us'" do
-      visit contact_path
       expect(page).to have_title("Gladio | Contact Us")
     end
   end
 
   describe "Help page" do
+    before { visit help_path}
 
-    it "should have the h1 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
+    it { should have_content('Help') }
 
     it "should have the title 'Help'" do
-      visit help_path
       expect(page).to have_title("Gladio | Help")
     end
   end
 
   describe "Home page" do
+    before { visit root_path}
 
-    it "should have the h1 'Gladio App'" do
-      visit root_path
-      expect(page).to have_content('Gladio App')
-    end
+    it { should have_content('Gladio App') }
 
     it "should have the title 'Home'" do
-      visit root_path
       expect(page).to have_title("Gladio | Home")
     end
   end
 
   describe "New page" do
+    before { visit new_path}
 
-    it "should have the h1 'New'" do
-      visit new_path
-      expect(page).to have_content('New')
-    end
+    it { should have_content('New') }
 
     it "should have the title 'New'" do
-      visit new_path
       expect(page).to have_title("Gladio | New")
     end
   end
