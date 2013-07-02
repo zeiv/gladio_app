5696b19621b026ffa656a2ca604ad29f4e831c6d
Trevor Jin <ph_201@yahoo.com>
2013-07-02 11:29:38 -0500

    Creates legitimate routes, eliminating static_pages from address URL. Makes Home the root route. Fixes #11.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 GladioApp::Application.routes.draw do
   get "questions/new"
-  get "static_pages/home"
-  get "static_pages/new"
-  get "static_pages/about"
-  get "static_pages/contact"
-  get "static_pages/help"
-  get "static_pages/blog"
+  root to: 'static_pages#home'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/blog', to: 'static_pages#blog', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/new', to: 'static_pages#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,78 +4,78 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+    it "should have the h1 'About Us'" do
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Gladio | About Us")
     end
   end
 
   describe "Blog page" do
 
-    it "should have the content 'Gladio Blog'" do
-      visit '/static_pages/blog'
+    it "should have the h1 'Gladio Blog'" do
+      visit blog_path
       expect(page).to have_content('Gladio Blog')
     end
 
     it "should have the title 'Blog'" do
-      visit '/static_pages/blog'
+      visit blog_path
       expect(page).to have_title("Gladio | Blog")
     end
   end
 
   describe "Contact page" do
 
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+    it "should have the h1 'Contact'" do
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Gladio | Contact Us")
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
+    it "should have the h1 'Help'" do
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Gladio | Help")
     end
   end
 
   describe "Home page" do
 
-    it "should have the content 'Gladio App'" do
-      visit '/static_pages/home'
+    it "should have the h1 'Gladio App'" do
+      visit root_path
       expect(page).to have_content('Gladio App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Gladio | Home")
     end
   end
 
   describe "New page" do
 
-    it "should have the content 'New'" do
-      visit '/static_pages/new'
+    it "should have the h1 'New'" do
+      visit new_path
       expect(page).to have_content('New')
     end
 
     it "should have the title 'New'" do
-      visit '/static_pages/new'
+      visit new_path
       expect(page).to have_title("Gladio | New")
     end
   end
