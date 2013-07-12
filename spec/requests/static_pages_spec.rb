9a7b89d8e622b2468b165338a323432be476e702
Trevor Jin <ph_201@yahoo.com>
2013-07-12 14:50:33 -0500

    Adds h1 selectors and adds full_title helper usage in tests. Fixes #23

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,60 +8,57 @@ describe "Static pages" do
   describe "About page" do
     before { visit about_path}
 
-    it { should have_content('About Us') }
+    it { should have_selector('h1',    text: 'About Us') }
 
     it "should have the title 'About Us'" do
-      expect(page).to have_title("#{base_title} | About Us")
+      expect(page).to have_title(full_title('About Us'))
     end
   end
 
   describe "Blog page" do
     before { visit blog_path}
 
-    it { should have_content('Gladio Blog') }
+    it { should have_selector('h1',    text: 'Gladio Blog') }
 
     it "should have the title 'Blog'" do
-      expect(page).to have_title("#{base_title} | Blog")
+      expect(page).to have_title(full_title('Blog'))
     end
   end
 
   describe "Contact page" do
     before { visit contact_path}
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1',    text: 'Contact') }
 
     it "should have the title 'Contact Us'" do
-      expect(page).to have_title("#{base_title} | Contact Us")
+      expect(page).to have_title(full_title('Contact Us'))
     end
   end
 
   describe "Help page" do
     before { visit help_path}
 
-    it { should have_content('Help') }
-
+    it { should have_selector('h1',    text: 'Help') }
     it "should have the title 'Help'" do
-      expect(page).to have_title("#{base_title} | Help")
+      expect(page).to have_title(full_title('Help'))
     end
   end
 
   describe "Home page" do
     before { visit root_path}
 
-    it { should have_content('Gladio App') }
-
+    it { should have_selector('h1',    text: 'Gladio App') }
     it "should have the title 'Home'" do
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title(full_title('Home'))
     end
   end
 
   describe "New page" do
     before { visit new_path}
 
-    it { should have_content('New') }
-
+    it { should have_selector('h1',    text: 'New') }
     it "should have the title 'New'" do
-      expect(page).to have_title("#{base_title} | New")
+      expect(page).to have_title(full_title('New'))
     end
   end
 end
