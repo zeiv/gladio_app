28a2091a1de571d4b9d1e981b49123e49e08545e
Trevor Jin <ph_201@yahoo.com>
2013-07-12 15:09:55 -0500

    Creates shared examples and optimizes the tests from unnecessary duplication. Fixes #16

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,60 +5,56 @@ describe "Static pages" do
   subject { page }
   let(:base_title) { "Gladio" }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "About page" do
     before { visit about_path}
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-    it { should have_selector('h1',    text: 'About Us') }
-
-    it "should have the title 'About Us'" do
-      expect(page).to have_title(full_title('About Us'))
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "Blog page" do
     before { visit blog_path}
+    let(:heading) { 'Gladio Blog' }
+    let(:page_title) { 'Blog' }
 
-    it { should have_selector('h1',    text: 'Gladio Blog') }
-
-    it "should have the title 'Blog'" do
-      expect(page).to have_title(full_title('Blog'))
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path}
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact Us' }
 
-    it { should have_selector('h1',    text: 'Contact') }
-
-    it "should have the title 'Contact Us'" do
-      expect(page).to have_title(full_title('Contact Us'))
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path}
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_selector('h1',    text: 'Help') }
-    it "should have the title 'Help'" do
-      expect(page).to have_title(full_title('Help'))
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "Home page" do
     before { visit root_path}
+    let(:heading) { 'Gladio App' }
+    let(:page_title) { 'Home' }
 
-    it { should have_selector('h1',    text: 'Gladio App') }
-    it "should have the title 'Home'" do
-      expect(page).to have_title(full_title('Home'))
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "New page" do
     before { visit new_path}
+    let(:heading) { 'New' }
+    let(:page_title) { 'New' }
 
-    it { should have_selector('h1',    text: 'New') }
-    it "should have the title 'New'" do
-      expect(page).to have_title(full_title('New'))
-    end
+    it_should_behave_like "all static pages"
   end
 end
