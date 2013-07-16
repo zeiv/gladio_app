dceb21c75095e20ccd26822ff773b89443d20dfa
Trevor Jin <ph_201@yahoo.com>
2013-07-16 17:13:01 -0500

    Adds password digests and tests.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130716215509) do
+ActiveRecord::Schema.define(version: 20130716221102) do
 
   create_table "answers", force: true do |t|
     t.string   "content"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20130716215509) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
