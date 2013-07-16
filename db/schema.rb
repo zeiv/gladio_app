49854b2a7fdad15389c289a8eed9636b69b91143
Trevor Jin <ph_201@yahoo.com>
2013-07-16 17:04:10 -0500

    Creates basic model tests. Fixes #34

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130716213733) do
+ActiveRecord::Schema.define(version: 20130716215509) do
 
   create_table "answers", force: true do |t|
     t.string   "content"
