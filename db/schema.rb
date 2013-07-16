5d1ba8d3676df2d4743fcbfa696e01aa1a41924d
Trevor Jin <ph_201@yahoo.com>
2013-07-16 16:43:07 -0500

    Creates user model. Fixes #30

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130702071901) do
+ActiveRecord::Schema.define(version: 20130716213733) do
 
   create_table "answers", force: true do |t|
     t.string   "content"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20130702071901) do
 
   create_table "questions", force: true do |t|
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
